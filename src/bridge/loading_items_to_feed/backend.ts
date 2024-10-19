19fa61258426406149a9bb8fec2ee31e95b928cd
Eivind Otto Hjelle <eohjelle@gmail.com>
2024-10-18 20:49:55 -0500

    Implemented a locking mechanism to avoid initializing a new feed if the previous one has not been properly closed. This should resolve a bug that was present in the Heroku deployment, but not the local one, due to higher latency.

--- src/bridge/loading_items_to_feed/backend.ts
+++ src/bridge/loading_items_to_feed/backend.ts
@@ -7,10 +7,14 @@ import type { Instructions } from './types';
 export abstract class StreamBackend<T = any> {
     protected stream: Map<T, ItemsStream>;
     protected reader: Map<T, ReadableStreamDefaultReader>;
+    protected lock: Map<T, Promise<void>>;
+    protected key: Map<T, () => void>; // A key that unlocks the lock
 
     constructor() {
         this.stream = new Map();
         this.reader = new Map();
+        this.lock = new Map();
+        this.key = new Map();
     }
 
     // In the subclass, the constructor also sets up listeners to handle three types of requests:
@@ -18,9 +22,16 @@ export abstract class StreamBackend<T = any> {
     // - closing the client connection (should call the closeClient method)
     // - requesting the next item (should call the nextItem method)
 
-    protected initClient(client: T, instructions: Instructions) {
+    protected async initClient(client: T, instructions: Instructions) {
         console.log('Initializing client stream with instructions', instructions);
         const stream = new ItemsStream(instructions);
+        if (this.lock.has(client)) {
+            console.log('Client already has a stream in progress. Waiting for it to finish...');
+            await this.lock.get(client);
+        }
+        this.lock.set(client, new Promise(resolve => {
+            this.key.set(client, resolve);
+        }));
         this.stream.set(client, stream);
         this.reader.set(client, stream.stream.getReader());
     }
@@ -30,6 +41,12 @@ export abstract class StreamBackend<T = any> {
         await this.reader.get(client)?.cancel();
         this.reader.delete(client);
         this.stream.delete(client);
+        const key = this.key.get(client);
+        this.key.delete(client);
+        this.lock.delete(client);
+        if (key) {
+            key();
+        }
     }
 
     protected async nextItem(client: T) {
